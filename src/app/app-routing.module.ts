9c74e52ae166fa119ddb47ee6087bb5decd81d73
Anish <gurunganish5@gmail>
2023-11-26 20:38:55 -0500

    my frontend crud application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewusersComponent } from './components/viewuser/viewuser.component';
+import { AdduserComponent } from './components/adduser/adduser.component';
+import { UpdateuserComponent } from './components/updateuser/updateuser.component';
 
-const routes: Routes = [];
+const routes: Routes =  [
+  { path: '', component: ViewusersComponent },
+  { path: 'add', component: AdduserComponent },
+  { path: 'update/:id', component: UpdateuserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AdduserComponent } from './components/adduser/adduser.component';
+import { UpdateuserComponent } from './components/updateuser/updateuser.component';
+import { ViewusersComponent } from './components/viewuser/viewuser.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdduserComponent,
+    UpdateuserComponent,
+    ViewusersComponent,
+    ViewusersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
